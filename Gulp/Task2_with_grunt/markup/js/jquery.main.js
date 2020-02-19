e6b8460647ebfd3fc9fc6c247c2ec7562b3190a5
Gekagmeka <shulgagggeka@gmail.com>
2020-02-19 23:00:49 +0200

    navigation mobile on|off

--- Gulp/Task2_with_grunt/markup/js/jquery.main.js
+++ Gulp/Task2_with_grunt/markup/js/jquery.main.js
@@ -1,5 +1,6 @@
-$('.burger-button').on('click',function(){
+$('.burger-button, .menu__item').on('click',function(){
 	$('.menu__navigation').toggleClass('active');
+	$('.burger-button__first-serif, .burger-button__second-serif, .burger-button__third-serif').toggleClass('active');
 	$('body').toggleClass('lock');
 	$(this).toggleClass('active');
 });
