08723baea69940d7ae3844f03c98c4b46d1f558e
Gekagmeka <shulgagggeka@gmail.com>
2020-03-22 19:57:44 +0200

    full header - desktop and mobile

--- Gulp/Task3_with_grunt/markup/js/jquery.main.js
+++ Gulp/Task3_with_grunt/markup/js/jquery.main.js
@@ -1,3 +1,4 @@
-$('.header__burger').on('click', function(){
+$('.header__burger, .header__item').on('click', function(){
 	$('.header__menu, .header__burger').toggleClass('active');
+	$('body').toggleClass('locked');
 });
