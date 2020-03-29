544f3074fbc568ee76a0433b9c29867e542c151a
Gekagmeka <shulgagggeka@gmail.com>
2020-03-29 22:25:56 +0300

    connecting the slick

--- Gulp/Task3_with_grunt/markup/js/jquery.main.js
+++ Gulp/Task3_with_grunt/markup/js/jquery.main.js
@@ -2,3 +2,10 @@ $('.header__burger, .header__item').on('click', function(){
 	$('.header__menu, .header__burger').toggleClass('active');
 	$('body').toggleClass('locked');
 });
+
+$('.carousel').slick({
+dots: true,
+infinite: true,
+slidesToShow: 2,
+slidesToScroll: 1
+});
