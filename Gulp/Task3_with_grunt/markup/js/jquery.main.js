3d029e352febe9cb208b6a04a90651f8df78d308
Gekagmeka <shulgagggeka@gmail.com>
2020-04-14 10:05:48 +0300

    arrow-up is ready - need 2 links near logo

--- Gulp/Task3_with_grunt/markup/js/jquery.main.js
+++ Gulp/Task3_with_grunt/markup/js/jquery.main.js
@@ -1,6 +1,28 @@
+// navigation
 $('.header__burger, .header__item').on('click', function(){
 	$('.header__menu, .header__burger').toggleClass('active');
 	$('body').toggleClass('locked');
+});
+
+// arrow-up function
+function Visibility(){
+	var scrollTop = $(window).scrollTop()
+	console.log(scrollTop)
+	if(scrollTop > 900){
+		$('.arrow-up').addClass('shifted')
+	}
+	else{
+		$('.arrow-up').removeClass('shifted')
+	}
+}
+Visibility();
+$(window).on('scroll', Visibility);
+
+//scroll to top
+$('.arrow-up').on('click', function(){
+	$('html').stop().animate({
+		scrollTop:0
+	});
 });
 
 $('.carousel').slick({
