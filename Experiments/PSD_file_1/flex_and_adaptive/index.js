1356599a477a88bc9f00916c0af02d488b61eb3d
Gekagmeka <shulgagggeka@gmail.com>
2020-08-02 18:19:52 +0300

    shop without preproc css

--- Experiments/PSD_file_1/flex_and_adaptive/index.js
+++ Experiments/PSD_file_1/flex_and_adaptive/index.js
@@ -1,5 +1,5 @@
 $('.burger-button').on('click', function(){ //добавляем событие при клике по burger-button и тогда NAV добавляем класс active
 $('nav').toggleClass('active')
-//также обычно добавляют класс active и самому нажатию (this)
+//также обычно добавляют класс active и самому нажатию (this) - если вдруг , ессть еще бургер баттон - то только по нажатию на этот бургер
 $(this).toggleClass('active')
 });
